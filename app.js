176597a2cb73c8d4855512238e7c97d092492259
Palmer Sturman <PalmersMac@palmers-mbp.lan>
2018-12-14 00:33:59 -0500

    Third update: blog views, database connect, post submissions

--- app.js
+++ app.js
@@ -3,14 +3,50 @@ const _ = require('lodash')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const request = require('request')
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(`public`))
 app.set(`view engine`, `ejs`)
 
+// mongoose.connect('REPLACE-ME-1', { useNewUrlParser: true })
+
+const postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: [ true, 'Please enter a post title.']
+    },
+    content: {
+        type: String,
+        required: [true, 'Please enter post content.']
+    },
+    tags: {
+        type: Array,
+        required: [true, 'Enter at least 1 post tag']
+    }
+})
+
+const Post = mongoose.model('Post', postSchema)
+
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/success', (req, res) => {
+    res.render('success')
+})
+
+app.get('/failure', (req, res) => {
+    res.render('failure')
+})
+
+app.get('/blog', (req, res) => {
+    res.render('blog')
+})
+
+app.get('/create-post', (req, res) => {
+    res.render('create-post')
 })
 
 app.post('/', (req, res) => {
@@ -32,12 +68,7 @@ app.post('/', (req, res) => {
     }
     const JSONData = JSON.stringify(data)
     const options = {
-        url: `https://us19.api.mailchimp.com/3.0/lists/99dc583253`,
-        method: `POST`,
-        headers: {
-            "Authorization": `terror1 17b6b62da4337a036729e72f6919dc9b-us19`
-        },
-        body: JSONData
+        // REPLACE-ME-2
     }
     
     request(options, (error, response, body) => {
@@ -51,6 +82,21 @@ app.post('/', (req, res) => {
             }
         }
     })
+})
+
+app.post("/create-post", (req, res) => {
+    const newPost = new Post ({
+        title: req.body.title,
+        content: req.body.content,
+        tags: req.body.tags
+    })
+
+    newPost.save((err, newPost) => {
+        if (err) console.log(err)
+        console.log(newPost)
+    })
+
+    res.render('success')
 })
 
 app.listen(3000, () => {
