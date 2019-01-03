951f9e7a894a8e30cd3ceb1e8933fed50272861f
Palmer Sturman <PalmersMac@Palmers-MacBook-Pro.local>
2019-01-03 13:08:41 -0500

    So many changes it's stupid

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require('express')
-const _ = require('lodash')
-const ejs = require('ejs')
-const bodyParser = require('body-parser')
-const request = require('request')
-const mongoose = require('mongoose')
+      _ = require('lodash')
+      ejs = require('ejs')
+      bodyParser = require('body-parser')
+      request = require('request')
+      mongoose = require('mongoose')
+      keys = require(`./config/keys`)
+      util = require(`util`)
+      formidable = require(`formidable`)
+      fs = require(`fs-extra`)
+      sizeOf = require(`image-size`)
+      Parser = require('rss-parser')
+      passport = require('passport')
+      Strategy = require('passport-local').Strategy
 
-const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(`public`))
-app.set(`view engine`, `ejs`)
 
-// mongoose.connect('REPLACE-ME-1', { useNewUrlParser: true })
+mongoose.connect(keys.connect.url, keys.connect.option)
 
 const postSchema = mongoose.Schema({
     title: {
         type: String,
-        required: [ true, 'Please enter a post title.']
+        required: [true, 'Please enter a post title.']
     },
     content: {
         type: String,
@@ -24,13 +28,40 @@ const postSchema = mongoose.Schema({
     tags: {
         type: Array,
         required: [true, 'Enter at least 1 post tag']
-    }
+    },
+    img: {
+        type: String,
+        required: [true, `Images must be 800 x 300px. Images cannot be larger than 10MB`]
+    },
+    timestamp: Date
 })
 
-const Post = mongoose.model('Post', postSchema)
+const userSchema = mongoose.Schema({
+    email: String,
+    password: String,
+    posts: [postSchema]
+})
+
+const tagsSchema = mongoose.Schema({
+    tags: Array
+})
+
+const PostModel = mongoose.model('Post', postSchema)
+const TagModel = mongoose.model('Tag', tagsSchema)
+const UserModel = mongoose.model('User', userSchema)
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(`public`))
+app.set(`view engine`, `ejs`)
 
 app.get('/', (req, res) => {
-    res.render('home')
+    const parser = new Parser()
+    const feed = parser.parseURL('https://feeds-cdn.buzzsprout.com/128078.rss')
+    PostModel.find({}, (err, posts) => {
+        if(err) console.log(err)
+        else res.render('home', { posts, feed })
+    })
 })
 
 app.get('/success', (req, res) => {
@@ -49,6 +80,30 @@ app.get('/create-post', (req, res) => {
     res.render('create-post')
 })
 
+app.get(`/post-success`, (req, res) => {
+    res.render('post-success')
+})
+
+app.get(`/post-failure`, (req, res) => {
+    res.render('post-failure')
+})
+
+app.get(`/login`, (req, res) => {
+    res.render('login')
+})
+
+app.get(`/post/:postId`, (req, res) => {
+    const requestedPostId = req.params.postId
+    PostModel.findOne({ _id: requestedPostId }, (err, post) => {
+        const postTitle = post.title
+        const postBody = post.content.replace(/\r\n\r\n/g, `\n`)
+        const postImg = post.img
+        const postDate = post.timestamp.toLocaleDateString('en-US')
+        if (err) console.log(err)
+        else res.render(`post`, {postTitle, postBody, postImg, postDate})
+    })
+})
+
 app.post('/', (req, res) => {
     const fName = req.body.fName
     const lName = req.body.lName
@@ -59,8 +114,8 @@ app.post('/', (req, res) => {
                 email_address: email,
                 status: 'subscribed',
                 merge_fields: {
-                    FNAME: req.body.fName,
-                    LNAME: req.body.lName
+                    FNAME: fName,
+                    LNAME: lName
                 }
 
             }
@@ -68,15 +123,21 @@ app.post('/', (req, res) => {
     }
     const JSONData = JSON.stringify(data)
     const options = {
-        // REPLACE-ME-2
+        url: `https://us19.api.mailchimp.com/3.0/lists/99dc583253`,
+        method: `POST`,
+        headers: {
+            "Authorization": keys.mailChimp
+        },
+        body: JSONData
     }
     
     request(options, (error, response, body) => {
         if (error) {
+            console.log(error)
             res.render(`failure`)
         } else {
             if (response.statusCode === 200) {
-                res.render(`success`)
+                res.render(`post-success`)
             } else {
                 res.render(`failure`)
             }
@@ -84,21 +145,53 @@ app.post('/', (req, res) => {
     })
 })
 
+app.post('/login', (req, res) => {
+    const form = new formidable.IncomingForm()
+    form.parse(req, (err, fields, files) => {
+        console.log(fields)
+    })
+})
+
 app.post("/create-post", (req, res) => {
-    const newPost = new Post ({
-        title: req.body.title,
-        content: req.body.content,
-        tags: req.body.tags
-    })
 
-    newPost.save((err, newPost) => {
-        if (err) console.log(err)
-        console.log(newPost)
-    })
+    const form = new formidable.IncomingForm()
+    form.uploadDir = `./public/files`
+    form.keepExtensions = true
+    form.maxFileSize = 10 * 1024 * 1024
 
-    res.render('success')
+    form.parse(req, (err, fields, files) => {
+
+        const tags = fields.tags.split(`,`)
+        const title = fields.title
+        const img = (imgPath) => {
+            const removePublic = imgPath.replace(`public/`, "")
+            return removePublic
+        }
+        const dimensions = sizeOf(files.file.path)
+        const content = fields.content
+        const newPost = new PostModel({
+            title,
+            content,
+            tags,
+            img: img(files.file.path),
+            timestamp: new Date()
+        })
+
+        if (err) {
+            res.redirect('post-failure-error', { postError: err })
+        } else {
+            if (dimensions.width === 800 && dimensions.height === 300) {
+                newPost.save((err) => {
+                    if (err) console.log(err)
+                    else res.redirect('/post-success')
+                })
+            } else {
+                res.redirect('/post-failure')
+            }
+        }
+    })
 })
 
 app.listen(3000, () => {
-    console.log(`We're live!`)
+    console.log(`Port 3000: We're live!`)
 })
